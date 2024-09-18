11a8668700cb79e4bf5ae235e3e201e49b85bc8d
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-09-18 11:27:13 +0300

    feat:update code to iniate discord client and migrate the database tables

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const envConfig = require("./configs/env.config");
-const DiscordManager = require("./managers/DiscordManager");
-const StatisticsManager = require("./managers/StatsManager");
-const StorageManager = require("./managers/StorageManager");
+const envConfig = require('./configs/env.config')
+const DiscordManager = require('./managers/DiscordManager')
+const StatisticsManager = require('./managers/StatsManager')
+const StorageManager = require('./managers/StorageManager')
 
 const storage = new StorageManager(envConfig)
-const statsManager = new StatisticsManager(envConfig , storage)
-const discordManager = new DiscordManager(envConfig , storage , statsManager)
+const statsManager = new StatisticsManager(envConfig, storage)
+const discordManager = new DiscordManager(envConfig, storage, statsManager)
 
-discordManager.init_client();
-// console.log(envConfig.discordToken);
+storage.migrate_db()
+discordManager.init_client()

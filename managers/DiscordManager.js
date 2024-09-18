0e970e65909351610913b1ac0834a7c9c9e06aef
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-09-18 11:25:39 +0300

    feat:DiscordManager watch for user messages and save it using StorageManager

--- managers/DiscordManager.js
+++ managers/DiscordManager.js
@@ -20,8 +20,10 @@ module.exports = class DiscordManager {
 
   init_client() {
     this.client.login(this.config.discordToken)
+    this.clientTag = ''
     this.client.on('ready', () => {
       this.logger.log_info(`client is ready and watching for messages > ${this.client.user.tag}`)
+      this.clientTag = this.client.user.tag
       this.watch_messages()
     })
   }
@@ -29,5 +31,26 @@ module.exports = class DiscordManager {
   watch_messages() {
     // on message event save save the event with user id
     // {message , user_id , username , time_created}
+    this.client.on('messageCreate', async (message) => {
+      const { username, id, displayName, tag } = message.author
+      const { createdTimestamp, content } = message
+      if (tag != this.clientTag) {
+        const reply = `${username} - ${displayName} - ${id}  - ${content} - ${createdTimestamp}`
+        await this.storage.save_message({
+          user_id: id,
+          username,
+          message_content: content,
+          time_created: createdTimestamp,
+        })
+        this.logger.log_info(reply)
+      }
+    })
   }
+
+  // TODO: Make slash commands for getting stats manually
+  // TODO: Permissions for commands
+  // LIKE
+  // /activity period?--> reply with same user activity
+  // /useractivity (user) period?  --> reply with supplied user activity [ADMIN]
+  // /allactivity period?
 }
--- managers/StorageManager.js
+++ managers/StorageManager.js
@@ -1,12 +1,72 @@
-const { Database } = require("sqlite3")
-const Logger = require("../utils/logger")
+const { Database } = require('sqlite3').verbose()
+const Logger = require('../utils/logger')
 
 module.exports = class StorageManager {
   constructor(config) {
-    this.client = new Database(config.dbPath)
     this.logger = new Logger('STORAGE')
+    this.client = new Database(config.dbPath, (err) => {
+      if (err) this.logger.log_err('Error opening database:', err.message)
+    })
   }
-  save_message({user_id , username , message_content , time_created}) {}
-  get_period_messages(start , end) {}
-  get_user_messages(user_id) {}
+
+  async migrate_db() {
+    const query = `
+      CREATE TABLE IF NOT EXISTS messages (
+        user_id INTEGER NOT NULL,
+        username TEXT NOT NULL,
+        message_content TEXT NOT NULL,
+        time_created INTEGER NOT NULL
+      );
+    `
+    await this.execute(query, [], 'Table "messages" created or already exists.')
+  }
+
+  async save_message({ user_id, username, message_content, time_created = Date.now() }) {
+    const query = `
+      INSERT INTO messages (user_id, username, message_content, time_created)
+      VALUES (?, ?, ?, ?);
+    `
+    await this.execute(query, [user_id, username, message_content, time_created], `Message saved for user ${username}`)
+  }
+
+  // FIXME: get only count
+  async get_period_messages(start, end) {
+    const query = `SELECT COUNT(*) FROM messages WHERE time_created BETWEEN ? AND ?;`
+    return await this.fetch(query, [start, end], `Messages retrieved between ${start} and ${end}`)
+  }
+
+  // FIXME: get only count of messages in specific timeframe
+  async get_user_messages(user_id) {
+    const query = `SELECT COUNT(*) FROM messages WHERE user_id = ? ORDER BY time_created DESC;`
+    return await this.fetch(query, [user_id], `Messages retrieved for user ${user_id}`)
+  }
+
+  // Helper methods for database operations
+  execute(query, params, successMsg) {
+    return new Promise((resolve, reject) => {
+      this.client.run(query, params, (err) => {
+        if (err) {
+          this.logger.log_err(err.message)
+          reject(err)
+        } else {
+          this.logger.log_info(successMsg)
+          resolve()
+        }
+      })
+    })
+  }
+
+  fetch(query, params, successMsg) {
+    return new Promise((resolve, reject) => {
+      this.client.all(query, params, (err, rows) => {
+        if (err) {
+          this.logger.log_err(err.message)
+          reject(err)
+        } else {
+          this.logger.log_info(successMsg)
+          resolve(rows)
+        }
+      })
+    })
+  }
 }

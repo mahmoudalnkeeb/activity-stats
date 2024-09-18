1874dc34e57410566f1ca77e5e4df7ba5e764f91
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-09-18 11:22:01 +0300

    refactor:support colors in logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,14 +1,25 @@
+const colors = {
+  Reset: '\x1b[0m',
+  Bright: '\x1b[1m',
+  Red: '\x1b[31m',
+  Green: '\x1b[32m',
+  Yellow: '\x1b[33m',
+}
+
 module.exports = class Logger {
   constructor(name) {
     this.name = name
   }
+
   log_err(message) {
-    console.error(`[${this.name}:ERR] ${message}`)
+    console.error(`${colors.Bright}${colors.Red}[${this.name}:ERR]${colors.Reset} ${message}`)
   }
+
   log_info(message) {
-    console.info(`[${this.name}:INFO] ${message}`)
+    console.info(`${colors.Bright}${colors.Green}[${this.name}:INFO]${colors.Reset} ${message}`)
   }
+
   log_warning(message) {
-    console.warn(`[${this.name}:WARN] ${message}`)
+    console.warn(`${colors.Bright}${colors.Yellow}[${this.name}:WARN]${colors.Reset} ${message}`)
   }
 }

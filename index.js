0fae8e07f5ede0e74e6eff9feee195e16f01ea5e
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-09-27 10:51:31 +0300

    feat(Logger):add the ability to log debug messages to logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const envConfig = require("./configs/env.config");
 const DiscordManager = require("./managers/DiscordManager");
 const StatisticsManager = require("./managers/StatsManager");
 const StorageManager = require("./managers/StorageManager");
+const Logger = require("./utils/logger");
 
 const storage = new StorageManager(envConfig);
 const statsManager = new StatisticsManager(envConfig, storage);
@@ -9,3 +10,7 @@ const discordManager = new DiscordManager(envConfig, storage, statsManager);
 
 storage.migrate_db();
 discordManager.init_client(process.argv.includes("reload-commands"));
+
+const mainLogger = new Logger("MAIN");
+
+mainLogger.log_debug("this is a test for debug message");
--- utils/logger.js
+++ utils/logger.js
@@ -4,11 +4,20 @@ const colors = {
   Red: "\x1b[31m",
   Green: "\x1b[32m",
   Yellow: "\x1b[33m",
+  Blue: "\x1b[34m",
+  Cyan: "\x1b[36m",
+  Magenta: "\x1b[35m",
 };
 
 module.exports = class Logger {
   constructor(name) {
     this.name = name;
+  }
+
+  log_debug(message) {
+    console.debug(
+      `${colors.Bright}${colors.Magenta}[${this.name}:DEBUG]${colors.Reset} ${message}`,
+    );
   }
 
   log_err(message) {
